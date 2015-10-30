abb46f07ee2b195167adb0764808aff092af4c67
Priit Tiganik <ptiganik@klass1-05-l.intra.itcollege.ee>
2015-10-30 14:45:54 +0200

    laevad grid

---
+++ src/teema2/LaevadGrid.java
@@ -0,0 +1,20 @@
+package teema2;
+
+/**
+ * Created by ptiganik on 30.10.15.
+ */
+public class LaevadGrid {
+    public static void main(String[] args) {
+        genereeriLaud();
+        //paigutaLaevad();
+        //kysiKasutajalt();
+        //kontrolliTabamust();
+        //kasOnLaevuAlles();
+        //gameover();
+    }
+
+    private static void genereeriLaud() {
+        System.out.println("jah");
+    }
+
+}
